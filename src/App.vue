a4b0291c1fab70e410de118cbfe791d6827616f0
夏菲杨 <838900155@qq.com>
2021-09-15 14:34:31 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <div>
+      <u-e :defaultMsg="defaultMsg" :config="config" id="uuee" ref="ue"></u-e>
+    </div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import UE from './components/ue/index.vue'
+export default {
+  name: 'App',
+  components: {
+    UE
+  },
+  data(){
+    return{
+      config: {
+        UEDITOR_HOME_URL: "/static/UE",
+        initialFrameWidth: "100%",
+        initialFrameHeight: 300
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
